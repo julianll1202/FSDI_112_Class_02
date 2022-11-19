0c5c58684e59cf049e10be3ec6f09f9b423b1793
Julian Lopez <a20490712@itmexicali.edu.mx>
2022-11-19 13:30:40 -0800

    Assignment 03

--- pages/tests.py
+++ pages/tests.py
@@ -1,4 +1,5 @@
 from django.test import SimpleTestCase
+from django.urls import reverse
 
 
 class SimplePageTests(SimpleTestCase):
@@ -15,7 +16,17 @@ class SimplePageTests(SimpleTestCase):
         self.assertTemplateUsed(response, "base.html")
         self.assertTemplateUsed(response, "pages/home.html")
     
-    def test_home_uses_correct_templates(self):
+    def test_about_uses_correct_templates(self):
         response = self.client.get("/about/")
         self.assertTemplateUsed(response, "base.html")
+        self.assertTemplateUsed(response, "pages/about.html")
+    
+    def test_home_page_reverse_lookup(self):
+        response = self.client.get(reverse('home'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "pages/home.html")
+    
+    def test_about_page_reverse_lookup(self):
+        response = self.client.get(reverse('about'))
+        self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, "pages/about.html")
