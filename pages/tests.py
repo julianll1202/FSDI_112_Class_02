ae4388d437291923b8280d924e839051b6591f7a
Julian Lopez <a20490712@itmexicali.edu.mx>
2022-11-19 10:19:10 -0800

    Test pages

--- pages/tests.py
+++ pages/tests.py
@@ -1,3 +1,21 @@
-from django.test import TestCase
+from django.test import SimpleTestCase
 
-# Create your tests here.
+
+class SimplePageTests(SimpleTestCase):
+    def test_home_page_status_code(self):
+        response = self.client.get("/")
+        self.assertEqual(response.status_code, 200)
+    
+    def test_about_page_status_code(self):
+        response = self.client.get("/about/")
+        self.assertEqual(response.status_code, 200)
+    
+    def test_home_uses_correct_templates(self):
+        response = self.client.get("/")
+        self.assertTemplateUsed(response, "base.html")
+        self.assertTemplateUsed(response, "pages/home.html")
+    
+    def test_home_uses_correct_templates(self):
+        response = self.client.get("/about/")
+        self.assertTemplateUsed(response, "base.html")
+        self.assertTemplateUsed(response, "pages/about.html")
